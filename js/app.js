b9b0270b2eeb2b26308d6968cfa5c5fdb76ec17b
Thay <pattasiwa@gmail.com>
2021-02-02 13:47:24 -0500

    finished product, with modal and font awesome added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 $(() => {
 
-    $('li').click((event) => {
+    $('ul').on('click', 'li', (event) => {
         const $li = $(event.currentTarget)
         $li.toggleClass('completed')
 
     })
 
-    $('span').click((event) => {
+    $('ul').on('click', 'span', (event) => {
         const $span = $(event.currentTarget).parent()
 
         $span.fadeOut(500, () => {
@@ -14,6 +14,28 @@ $(() => {
         })
         event.stopPropagation()
 
+    })
+
+    $("input[type='text'").keypress(function (event) {
+        if (event.which === 13) {
+            const $newTask = $(this).val()
+            $(this).val('')
+
+            $('ul').append("<li><span class='deleteSpan'><i class='fa fa-ban' aria-hidden='true'></i></span> " + $newTask + "</li>")
+
+
+        }
+    })
+
+    //get modal element
+
+    $('#modalBtn').on('click', (event) => {
+        console.log('hello')
+        $('.modal').css('display', 'block')
+    })
+
+    $('.closeBtn').on('click', () => {
+        $('.modal').css('display', 'none')
     })
 
 
